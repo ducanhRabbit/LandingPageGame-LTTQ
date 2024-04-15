dcd89d2670649c81df2d15f0f25bc3885000f427
ducanhRabbit <renyk97@gmail.com>
2024-04-15 17:41:54 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import Test from "./component/test/Test"
+
+import Test from "./component/test/Test"
+
+
+function App() {
+
+
+  return (
+    <>
+       <Test/>
+    </>
+  )
+}
+
+export default App
